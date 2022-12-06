7f7502dfeb38d3c63afd01a4b5e8eb97e6bff935
Latschesar Atanassov <latschesar.atanassov@nordcloud.com>
2022-12-06 09:57:37 +0000

    Add ecs cluster

---
+++ terraform/compute.tf
@@ -0,0 +1,43 @@
+module "ecs" {
+  source = "terraform-aws-modules/ecs/aws"
+
+  cluster_name = local.cluster_name
+
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = aws_cloudwatch_log_group.this.name
+      }
+    }
+  }
+
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+  }
+
+  tags = {
+    Name        = "${local.name_prefix}_ecs"
+    Environment = var.environment
+  }
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/ecs/${local.cluster_name}"
+  retention_in_days = 7
+
+
+  tags = {
+    Name        = "${local.name_prefix}_cw"
+    Environment = var.environment
+  }
+}
