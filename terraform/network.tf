64f49c12b7d28f8761610f7278d9f241071bfa50
Latschesar Atanassov <latschesar.atanassov@nordcloud.com>
2023-01-09 15:17:06 +0100

    Set private route table

--- terraform/network.tf
+++ terraform/network.tf
@@ -224,6 +224,9 @@ resource "aws_route_table_association" "private_snet_data_c_rta" {
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.main.id
   service_name = "com.amazonaws.eu-central-1.s3"
+  route_table_ids = [
+    aws_route_table.private.id
+  ]
 }
 
 resource "aws_vpc_endpoint" "cloudwatch" {
